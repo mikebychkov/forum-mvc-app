d2518c8448bd50666a94eba6f2b80830681299ab
Mike <mikebychkov@github.com>
2023-03-01 05:54:41 +0500

    k8s image pull error workaround

--- my-local-registry/user-host-config.sh
+++ my-local-registry/user-host-config.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+
+# FOR DOCKER USERS
 sudo mkdir -p /etc/docker/certs.d/my.local.registry/
 sudo cp ./certs/domain.crt /etc/docker/certs.d/my.local.registry/ca.crt
+
+# FOR K8S NODES WITH CONTAINERD
+sudo cp ./certs/domain.crt /etc/ssl/certs/domain.crt
